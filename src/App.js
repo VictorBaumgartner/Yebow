314d4698817899dfb6a4cdf391327f43f695bc0f
THEROS22 <52211682+VictorBaumgartner@users.noreply.github.com>
2023-06-25 11:36:16 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello world !</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <p>hey, my name is Victor and I work on this project.</p>
     </div>
   );
 }
